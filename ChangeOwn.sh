b2ac78927da31ce6865f1b0de135adf3af6a0bf7
soeasyright <soeasyright@superdb2.com>
2013-07-09 15:06:04 +0800

    Chage cocos2dRoot

--- ChangeOwn.sh
+++ ChangeOwn.sh
@@ -1,9 +1,14 @@
 origin="cocos2d-2.1rc0-x-2.1.3"
-target="cocos2d-x-2.1.4"
-    
+for (( i=1; i<=10; i=i+1 ))
+do
+        tmp=`echo $COCOS2DX_ROOT | cut -d '/' -f$i | grep cocos`
+	        if [ "$tmp" != "" ];then
+			target=$tmp;
+		fi
+done
+#target="cocos2d-x-2.1.4"
 origin2=atwood
 target2=`pwd |cut -d '/' -f3`
-  
 for varown in atwood double2 David deak
 do
 for varfile in jni/Android.mk .cproject .externalToolBuilders/NDK_Builder.launch .settings/org.eclipse.cdt.core.prefs
